35a21a4d28d7a06c26f82299259b549e42767a16
rafaelortet <rafael.ortet@84000.co>
2026-03-11 11:09:45 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+
+export const metadata = {
+    title: "Bodhi Central — Documentation",
+    description: "The Bodhi Central repository of guidelines, documentation, and platform technical information.",
+}
+
+const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const navbar = (
+    <Navbar
+        logo={<b>Bodhi Central</b>}
+    // ... Your additional navbar options
+    />
+)
+const footer = <Footer>Bodhi Central {new Date().getFullYear()} © All rights reserved.</Footer>
+
+export default async function RootLayout({ children }) {
+    return (
+        <html
+            // Not required, but good for SEO
+            lang="en"
+            // Required to be set
+            dir="ltr"
+            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+            suppressHydrationWarning
+        >
+            <Head
+            // ... Your additional head options
+            >
+                {/* Your additional tags should be passed as `children` of `<Head>` element */}
+            </Head>
+            <body>
+                <Layout
+                    banner={banner}
+                    navbar={navbar}
+                    pageMap={await getPageMap()}
+                    docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+                    footer={footer}
+                // ... Your additional layout options
+                >
+                    {children}
+                </Layout>
+            </body>
+        </html>
+    )
+}
